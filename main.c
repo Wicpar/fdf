bf43bc75155dd44158f5632e2236ca093faaa423
Frederic NIETO <fnieto@e3r1p2.42.fr>
2016-01-06 13:18:13 +0100

    dsd

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fnieto <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/05 12:36:19 by fnieto            #+#    #+#             */
-/*   Updated: 2016/01/05 13:12:44 by fnieto           ###   ########.fr       */
+/*   Updated: 2016/01/06 13:09:46 by fnieto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 #include "libft.h"
 #include "mlx.h"
 
+#include <stdio.h>
+#include <math.h>
+
 int		main(int ac, char **av)
 {
 	if (ac == 2)
 		ft_putendl(av[1]);
+	double i = 0;
+	while (i < PI * 4)
+	{
+		printf("%lf : %lf\n", sin(i), SIN(i));
+		i += 0.01;
+	}
 }
