a76bb6ef33c3b56f8cfcc86298f86d6c4bf25584
Frederic NIETO <fnieto@e3r3p12.42.fr>
2016-01-19 17:07:05 +0100

    gtg

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fnieto <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/05 12:36:19 by fnieto            #+#    #+#             */
-/*   Updated: 2016/01/18 23:47:22 by fnieto           ###   ########.fr       */
+/*   Updated: 2016/01/19 17:05:58 by fnieto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,84 +23,6 @@ void		*g_mlx_window_main;
 void		*g_mlx_frame;
 t_param		g_params;
 t_mat4		g_camera;
-
-int			uv_shader(t_shader_info params)
-{
-	int		col;
-	t_vec2	uv = div_vec2(params.i_frag_coord, params.i_resolution);
-	col = (encode(uv.x, uv.y, FRACT(params.i_global_time)));
-	return (col);
-}
-
-int			plasma_shader(t_shader_info i)
-{
-	t_vec2 p = add_vec2_1(mul_vec2_1(div_vec2(i.i_frag_coord, i.i_resolution), 2), -1);
-
-	t_float x = p.x;
-	t_float y = p.y;
-	t_float mov0 = x+y+cos(sin(i.i_global_time)*2.0)*100.+sin(x/100.)*1000.;
-	t_float mov1 = y / 0.9 +  i.i_global_time;
-	t_float mov2 = x / 0.2;
-	t_float c1 = fabs(sin(mov1+i.i_global_time)/2.+mov2/2.-mov1-mov2+i.i_global_time);
-	t_float c2 = fabs(sin(c1+sin(mov0/1000.+i.i_global_time)+sin(y/40.+i.i_global_time)+sin((x+y)/100.)*3.));
-	t_float c3 = fabs(sin(c2+cos(mov1+mov2+c2)+cos(mov2)+sin(x/1000.)));
-	return (encode(c1, c2, c3));
-}
-
-int			circle_shader(t_shader_info i)
-{
-	t_vec2 p = div_vec2_1(sub_vec2(mul_vec2_1(i.i_frag_coord, 2), i.i_resolution), i.i_resolution.y);
-	t_float tau = PI*2.0;
-	t_float a = atan2(p.x,p.y);
-	t_float r = len_vec2(p)*0.75;
-	t_vec2 uv = vec2(a/tau,r);
-	t_float xCol = (uv.x - (i.i_global_time / 3.0)) * 3.0;
-	xCol = MOD(xCol, 3.0);
-	t_vec3 horColour = vec3(0.25, 0.25, 0.25);
-
-	if (xCol < 1.0) {
-
-		horColour.x += 1.0 - xCol;
-		horColour.y += xCol;
-	}
-	else if (xCol < 2.0) {
-
-		xCol -= 1.0;
-		horColour.y += 1.0 - xCol;
-		horColour.z += xCol;
-	}
-	else {
-
-		xCol -= 2.0;
-		horColour.z += 1.0 - xCol;
-		horColour.x += xCol;
-	}
-	uv.x = (2.0 * uv.x) - 1.0;
-	uv.y = (2.0 * uv.y) - 1.0;
-	t_float beamWidth = (0.7+0.5*cos(uv.x*10.0*tau*0.15*CLAMP(FLOOR(5.0 + 10.0*cos(i.i_global_time)), 0.0, 10.0))) * fabs(1.0 / (30.0 * uv.y));
-	t_vec3 horBeam = vec3_1(beamWidth);
-	return (encode_vec3(mul_vec3(horBeam, horColour)));
-}
-
-int			sphere_shader(t_shader_info i)
-{
-	t_vec4 f;
-	t_vec3 r = vec3(cos(i.i_global_time) + 1, 2, sin(i.i_global_time) + 1),
-	  R = vec3(i.i_resolution.x, i.i_resolution.y, 1);
-
-	t_float	u = .6;
-	while (u > .1)
-	{
-		r = add_vec3(r, mul_vec3_1(mul_vec3_1(vec3((2 * i.i_frag_coord.x - R.x)/R.y, (2 * i.i_frag_coord.y - R.y)/R.y, 2 ), .06), (f.x = len_vec3(vec3(FRACT(r.x) - .5, FRACT(r.y) - .5, FRACT(r.z) - .5)) - .3)));
-		f.z =u*r.x;
-		f.y =u*r.y;
-		f.x =u*r.z;
-		if( f.w < .001 ) break ;
-		u -= .004;
-	}
-	f.w = 0;
-	return (encode_vec4(f));
-}
 
 int			draw_px(int x, int y, int color)
 {
@@ -132,10 +54,6 @@ int			loop(void *param)
 		b = vertex(vec3(g_params.res.x, (t_float)i, 0), def);
 		draw_line(a, b, &sphere_shader, get_instance()->frame);
 	}
-	//	a = vertex(vec3(0, g_instance->frame->h / 2., 0), def);
-	//	b = vertex(vec3(g_params.res.x, MOD(g_global_time * 60, g_instance->frame->h), 0), def);
-	//	draw_line(a, b, &uv_shader, g_instance->frame);
-
 	set_time(get_time() + 0.1);
 	mlx_put_image_to_window(g_mlx_core, g_mlx_window_main, g_mlx_frame, 0, 0);
 	return ((int)param);
--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: fnieto <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/07 11:40:11 by fnieto            #+#    #+#             */
-/*   Updated: 2016/01/08 11:38:22 by fnieto           ###   ########.fr       */
+/*   Updated: 2016/01/19 17:03:10 by fnieto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,98 +15,34 @@
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-static t_list	*file_to_lst(char *file)
+t_vec2		map_dim(char *file)
 {
-	t_list		*lst;
-	int			fd;
-	char		*ln;
-	
+	int		fd;
+	t_vec2	dim;
+	size_t	cnt;
+	char	*tmp;
+	char	*i;
+
+	dim = vec2(1, 0);
 	fd = open(file, O_RDONLY);
-	lst = 0;
-	if (fd < 0)
-		puterr(1, "File not Found or in use /!\\");
-	while (get_next_line(fd, &ln) > 0)
-		ft_lstadd(&lst, ft_lstnew(ln, ft_strlen(ln) + 1));
+	while (get_next_line(fd, &tmp) > 0)
+	{
+		i = tmp;
+		cnt = 0;
+		while (*i)
+		{
+			if (*i && *i == ' ' && i[1] && i[1] != ' ')
+				cnt++;
+		}
+		free(tmp);
+		if (dim.x == 0)
+			dim.x = cnt;
+		else if (dim.x > 0 && dim.x != cnt)
+			dim.x = -1;
+		dim.y++;
+	}
 	close(fd);
-	return (lst);
-}
-
-static void		bufferize(t_buffer *buf, t_list *lst)
-{
-	size_t	i;
-	size_t	j;
-	char	**temp;
-	t_list	*tmp;
-
-	tmp = lst;
-	j = -1;
-	while (tmp)
-	{
-		++j;
-		i = -1;
-		temp = ft_strsplit(tmp->content, ' ');
-		while (temp[++i])
-		{
-			buff_set(buf, i, j, &temp[i]);
-		}
-		tmp = tmp->next;
-	}
-}
-
-t_buffer		*file_to_strbuff(char *file)
-{
-	size_t		i[2];
-	t_list		*lst[2];
-	char		**ln;
-	t_buffer	*buf;
-
-	lst[0] = file_to_lst(file);
-	lst[1] = lst[0];
-	i[0] = 0;
-	while ((lst[1]) != 0)
-	{
-		lst[1] = (lst[1])->next;
-		i[0]++;
-	}
-	i[1] = 0;
-	ln = ft_strsplit(lst[0]->content, ' ');
-	while (ln[i[1]])
-	{
-		ft_memdel((void**)&ln[i[1]]);
-		++i[1];
-	}
-	ft_memdel((void**)ln);
-	buf = buff_new(i[1], i[0], sizeof(char*));
-	bufferize(buf, lst[0]);
-	return (buf);
-}
-
-t_buffer		*strbuff_to_vertbuff(t_buffer *buf)
-{
-	size_t		i[2];
-	t_buffer	*new;
-	t_vertex	tmp;
-	char		**sep;
-
-	new = buff_new(buf->w, buf->h, sizeof(t_vertex));
-	ft_putendl("b");
-	i[0] = -1;
-	while (++i[0] < buf->h)
-	{
-		i[1] = -1;
-		while (++i[1] < buf->w)
-		{
-			sep = ft_strsplit(*((char**)buff_get(buf, i[1], i[0])), ',');
-			tmp.pos = vec_new(i[1] - (t_float)buf->w / 2,
-					i[0] - (t_float)buf->w / 2, ft_atoi(sep[0]), 0);
-			tmp.color = sep[1] ?
-				ft_atoi_base(sep[1] + 2, "0123456789ABCDEF") : 0xFFFFFF;
-			buff_set(new, i[1], i[0], &tmp);
-			ft_memdel((void**)&sep);
-		}
-	}
-	ft_putendl("b");
-	buff_del(&buf);
-	return (new);
+	return (dim);
 }
